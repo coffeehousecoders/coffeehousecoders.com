61ba3cd541571f2ef8a0d1310a0ed84fcac972ea
Kevin Champion <kevin.champion@gmail.com>
2012-10-03 21:38:27 -0400

    Update zach, adam images. Remove jquery dependency in retina.js.

--- public/javascripts/retina.js
+++ public/javascripts/retina.js
@@ -2,23 +2,51 @@
 // Only replaces images with the class hi-res.
 // @source http://www.kylejlarson.com/blog/2012/creating-retina-images-for-your-website/
 
-$(function () {
+(function () {
 
   if (window.devicePixelRatio == 2) {
 
-          var images = $("img.hi-res");
+    var images = document.getElementsByTagName('img');
 
-          // loop through the images and make them hi-res
-          for(var i = 0; i < images.length; i++) {
+    // Loop through the imgs.
+    for (var i = 0; i < images.length; i++) {
 
-            // create new image name
-            var imageType = images[i].src.substr(-4);
-            var imageName = images[i].src.substr(0, images[i].src.length - 4);
-            imageName += "@2x" + imageType;
+      var classes = images[i].className.split(' ');
 
-            //rename image
-            images[i].src = imageName;
+      for (key in classes) {
+
+        if (classes[key] == 'hi-res') {
+
+          // First check and removed any ? or # at end of src path.
+          var questionPosition = images[i].src.lastIndexOf('?');
+          var poundPosition = images[i].src.lastIndexOf('#');
+          var periodPosition = images[i].src.lastIndexOf('.');
+
+          if (questionPosition > periodPosition) {
+
+            images[i].src = images[i].src.substr(0, questionPosition);
+
           }
-     }
 
-});
+          if (poundPosition > periodPosition) {
+
+            images[i].src = images[i].src.substr(0, poundPosition);
+
+          }
+
+          var imageType = images[i].src.substr(-4);
+          var imageName = images[i].src.substr(0, images[i].src.length - 4);
+          imageName += "@2x" + imageType;
+
+          // Rename image.
+          images[i].src = imageName;
+
+        }
+
+      }
+
+    }
+
+  }
+
+})();
